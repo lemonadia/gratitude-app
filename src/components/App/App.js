e7b680e1a64e2562fc631ffe812cb82260a041f8
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2020-03-18 17:09:00 +0100

    Add background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>Simple gratitude App!</h1>
+        <h1 className={styles.title}>Simple gratitude App</h1>
         <h2 className={styles.subtitle}>What made you smile today?</h2>
       </main>
     );
