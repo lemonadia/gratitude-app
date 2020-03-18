e01ee8f53f25b2848011d2e099c5ba7e9697e55a
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2020-03-18 16:57:51 +0100

    Add code to App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./App.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className={styles.component}>
+        <h1 className={styles.title}>Simple gratitude App!</h1>
+        <h2 className={styles.subtitle}>What made you smile today?</h2>
+      </main>
+    );
+  }
+}
+
+export default App;
