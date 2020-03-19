64577682d2613c7411b7a21bbbc0c2f4df2e62a2
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2020-03-19 21:59:44 +0100

    Add Card component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import ReactHtmlParser from "react-html-parser";
 
 const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>{ReactHtmlParser(props.title)}</h2>
+    {/* <h2 className={styles.title}>{ReactHtmlParser(props.title)}</h2> */}
   </header>
 );
 
