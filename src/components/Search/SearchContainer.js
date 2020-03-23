1ae97c29e5e152a249b6f60b65895b64d1a9a003
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2020-03-23 21:07:05 +0100

    Add search functionality

--- src/components/Search/SearchContainer.js
+++ src/components/Search/SearchContainer.js
@@ -1,19 +1,15 @@
-import {connect} from 'react-redux';
-import Search from './Search';
-import {
-  getSearchString,
-  countVisibleCards,
-  countAllCards,
-  createAction_changeSearchString,
-} from '../../redux/searchStringRedux';
+import { connect } from "react-redux";
+import Search from "./Search";
+import { getSearchString, countVisibleCards, countAllCards, createAction_changeSearchString } from "../../redux/searchStringRedux";
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   searchString: getSearchString(state),
   countVisible: countVisibleCards(state),
   countAll: countAllCards(state),
+
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
 });
 
